cd03d32c81b5105244d73382f1875439058cd6b9
kyoncy <kyota.n.2014@gmail.com>
2020-11-13 07:22:50 +0900

    add eslint rules prettier/prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,14 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    'prettier',
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
     'prettier/@typescript-eslint'
   ],
+  rules: {
+    "prettier/prettier": "error",
+  },
 };
