e295fb5a7736f0bb330dd8f1682867731192ff79
kyoncy <kyota.n.2014@gmail.com>
2020-11-15 13:55:47 +0900

    add material-ul

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,10 @@ const Home: NextPage<HomeProps> = ({ audioFile }) => {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
